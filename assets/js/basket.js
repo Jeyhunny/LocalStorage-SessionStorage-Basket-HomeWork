2e6fbc9a7111e5cd23a000b2fda4262f650d0622
unknown <jeyhunny@code.edu.az>
2023-02-26 23:12:58 +0400

    two commit

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,9 +1,9 @@
 "use strict"
 
 
-let tableBody = document.querySelector("tbody");  //table bodysini goturmek uchun
+let tableBody = document.querySelector("tbody");
 
-let products = JSON.parse(localStorage.getItem("basket"));  // localda olan productlari goturmek uchun
+let products = JSON.parse(localStorage.getItem("basket"));
 
 
 if (products != null) {
@@ -21,7 +21,7 @@ if (products != null) {
         <td><i class="fa-solid fa-trash-can"></i></td>
         </tr>`
     }
-    getBasketCount(products); //basketdeki mehsullarin sayini gostermek uchun hemin metodun ichine products arrayini gonderirik
+    getBasketCount(products);
     totalPrice(products);
 
 } else {
@@ -38,7 +38,7 @@ function getBasketCount(arr) {
     for (const item of arr) {
         sum += item.count
     }
-    //document.querySelector("sup").innerText = arr.length;   //baskete atdomiz mehsulun sayini gostermek uchun
+
 
     document.querySelector("sup").innerText = sum;
 }
@@ -53,24 +53,23 @@ let deleteIcons = document.querySelectorAll("tbody tr td i");
 
 for (const deleteIcon of deleteIcons) {
     deleteIcon.addEventListener("click", function () {
-        let newList = products.filter(m => m.id != deleteIcon.parentNode.previousElementSibling.innerText)  //yeni array yaradiram hansiki ichinde silmek istediyim id-ye aid olan productdan bawqa  productlar olsun
-        //burada m => m.id localda olan id nezerde tutulur. (deleteIcon.parentNode.previousElementSibling.innerText)----- bu ise silmek uchun olan iconun yaninda olan mehsulun idsidi. yeni visualda gorsenen id
-        //yeni icona basanda silmek istediyimiz  productun id-sine beraber olan id li product varsa localda, sil ve o id-li product olmadan yeni product listi yarat ve locala yeniden elave et. 
-        //silmek istediyimiz productun iconuna  basdigimizda hemin productdan bawqa digerlerini gostersin.
+        let newList = products.filter(m => m.id != deleteIcon.parentNode.previousElementSibling.innerText)
 
-        products = newList;   // evvelki products listinin adini yeni yaratdigimiz arrayin adina beraber ediremki. arrayin ichindekiler silinenden bawqa digerleri olsun.
 
-        localStorage.setItem("basket", JSON.stringify(products))   //local storageni yeniden set edirik ki, sildiyimiz product hemin listde olmasin. local storagede yeni list olsun.
 
-        window.location.reload();    //refresh ediremki silinen datalar UI da qalmasin. bele etmesek silmek istediyimiz silinecek amma ekranda helede gorsenecek
+        products = newList;   
 
-        if (products.length == 0) {    //eger storagede data yoxdursa, yeni localimizda productumuzun(basketimizin) length sifirdirsa localdan productumuzun keyini oradan sil.
+        localStorage.setItem("basket", JSON.stringify(products))   
+
+        window.location.reload();   
+
+        if (products.length == 0) {   
             localStorage.clear();
         }
     })
 }
 
-// let newList = products.splice(i,1)   //indexe gore productlari silmek uchun istifade edirik. hansina click edirikse onu silmesi uchun
+// let newList = products.splice(i,1)  
 
 
 
@@ -78,16 +77,16 @@ for (const deleteIcon of deleteIcons) {
 function totalPrice(arr) {
     let sum = 0;
     for (const item of arr) {
-        sum += parseInt(item.price) * parseInt(item.count);         //mehsulun qiymetini vur sayina her defe topla.
+        sum += parseInt(item.price) * parseInt(item.count);       
     }
-    document.querySelector("#basket .total h3").innerHTML = `<span>Total: ${sum} AZN </span>`;   //umumi neticeni h3 - yeni total olan divin innerHTMLsine  beraber et
+    document.querySelector("#basket .total h3").innerHTML = `<span>Total: ${sum} AZN </span>`;   
 }
 
 
 
 //clear all products 
 let clearBtn = document.querySelector(".clear-btn");
-clearBtn.addEventListener("click",function(){
+clearBtn.addEventListener("click", function () {
     localStorage.clear();
     window.location.reload();
 })
@@ -121,14 +120,14 @@ let plusIcons = document.querySelectorAll("tbody tr td .plus")
 for (const plusIcon of plusIcons) {
     plusIcon.addEventListener("click", function () {
         for (const product of products) {
-            if (product.id == plusIcon.parentNode.nextElementSibling.innerText) {  //icon olan hissede olan (ve gorsenmeyen) idni tapiriq.
-                plusIcon.previousElementSibling.innerText++;                       //iconun ust elementi yeni count un inner textini ++ et yeni nechedirse artir
-                product.count++;                                                  //productun countunu artir    
-                totalPrice(products)                                               //total a productun son qiymetini gonder
+            if (product.id == plusIcon.parentNode.nextElementSibling.innerText) {  
+                plusIcon.previousElementSibling.innerText++;                      
+                product.count++;                                                  
+                totalPrice(products)                                               
                 window.location.reload();
             }
         }
-        localStorage.setItem("basket", JSON.stringify(products))                   //butun bu deyishiklikleri locala elave et
+        localStorage.setItem("basket", JSON.stringify(products))                   
 
     })
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,46 +4,46 @@
 
 
 // localStorage.setItem("name","Cavid");
-// //localStorage.setItem local strage-e data elave etmek uchun istifade olunur. key value mentiqi kimi. key-name, value-cavid
+
 // localStorage.setItem("surname", "Ismayilzade")
 
 // // console.log(localStorage.getItem("name"));
-// // //getItem elave etdiyimiz datalari goturmek uchun keyini yaziriq valueni gosterir
+
 
 
 
 // // localStorage.removeItem();
-// //datalari silmedikce localstoragede qalacaq. silme isteyirikse elimizle remova methodunu yaza bilerik
+
 
 
 // let names = ["Pervin","Elekber","Aqshin"]
 
 // // localStorage.setItem("name", names);
-// //bu halda arrayi string kimi elave edir storageye.
+
 
 
 // localStorage.setItem("name", JSON.stringify(names));
-// //bir arrayi local storageye array formatinda qoymaq isteyirikse, JSON.stringfy ile arrayi array formatinda local storageye qoya bilirsen
+
 
 
 // console.log(JSON.parse(localStorage.getItem("names")));
-// //locala goyulmush arrayi goturende JSON.parse edib goture bilerik
+
 
 // document.querySelector("button").onclick = function(){
 //     // localStorage.removeItem("name");
-//     // bu halda keyine gore elementi silirik
+//     
 
 //     // localStorage.clear();
-//     // local storagede olan herweyi silir
+//    
 
 
 //     let datas = JSON.parse(localStorage.getItem("names")); 
-//     //parse edib goturduyumuz arrayi variable a beraber edirik
+//     
 
 //     for (const item of datas) {
 //         console.log(item);
 //     }  
-//     //arrayin ichindeki datalari birbir gosteririk
+//     
 
 // }
 
@@ -62,36 +62,36 @@
 
 let cardBtns = document.querySelectorAll("#shop a")
 
-let products = [];   //ilk once bow array teyin edirikki sonradan bir-bir productlari elave edek bunun icherisine
+let products = [];  
 
-if(localStorage.getItem("basket") != null){                   //  local storagede mehsul varsa yeni null deyilse 
-    products = JSON.parse(localStorage.getItem("basket"))     //elave edtiyimiz yeni mehsullari (yeni bow olan arrayin ichine elave etdiklerimizi) elave et local storagede olanlarin uzerine
-    //datani array kimi goturmek uchun JSON.parse edib gotururuk
+if(localStorage.getItem("basket") != null){                   
+    products = JSON.parse(localStorage.getItem("basket"))    
+  
 
 }
 
 cardBtns.forEach(btn => {
-    btn.addEventListener("click", function (e) {              //burada buttona click edende bir-bir cardlari goture bilmek uchun
+    btn.addEventListener("click", function (e) {              
 
-        e.preventDefault();                                    //cardin, buttonun defaultunu silmek uchun
+        e.preventDefault();                                   
 
-        let productImg = this.parentNode.previousElementSibling.getAttribute("src");  //buttona click edende img i goturmek uchun. buttonun parent divinin ust siblingi deyib gotururuk
+        let productImg = this.parentNode.previousElementSibling.getAttribute("src");  
         let productName = this.parentNode.firstElementChild.innerText;
         let productDesc = this.previousElementSibling.previousElementSibling.innerText;
         let productPrice = this.previousElementSibling.innerText;
         let productId = parseInt(this.parentNode.parentNode.getAttribute("data-id"));
 
         let exitProduct = products.find(m => m.id == productId)
-        //click edende, yeni eleave etdiyimiz productun idsi ile eyni olan idli product varsa onu yoxlayiriq
+     
        
         if(exitProduct != undefined){
             exitProduct.count +=1;
         }else{
             products.push({
-                // buttona her click edende elave et productsun ichine click etdiyimiz her bir productu.
+                
                 
                 id:productId,
-                name: productName,        // anonim obejct yaradic propertilerini elave edirik ichine
+                name: productName,        
                 img: productImg,
                 description: productDesc,
                 price:productPrice,
@@ -101,12 +101,10 @@ cardBtns.forEach(btn => {
 
         getBasketCount(products);
 
-        //JSON FRONTDAN BACKA DATA OTURMEK UCHUNDUR. frontla back arasinda data transferi uchun istifade edir
-        //stringify --string formatini array formatina chevirib yaratmaq uchun
-        //parse --  datani array (obyekt) kimi goturmek isteyirikse parse edirik. 
+      
 
         localStorage.setItem("basket", JSON.stringify(products));
-        //local storageye productslarini array kimi elave etmek uchun(yeni set elemek), JSON.stringFy edib elave edirik
+      
         
     })
 });
@@ -118,9 +116,9 @@ function getBasketCount(arr){
     for (const item of arr) {
         sum+=item.count
     }
-    //document.querySelector("sup").innerText = arr.length;   //baskete atdomiz mehsulun sayini gostermek uchun. yeni bir basketde neche nov m,ehsul var. her mehsuldan neche dene oldugunu gostermesin.
+    //document.querySelector("sup").innerText = arr.length;   
 
-    document.querySelector("sup").innerText = sum;  //basketde butun mehsullarin sayi gostersin
+    document.querySelector("sup").innerText = sum; 
 }
 
 getBasketCount(products);
